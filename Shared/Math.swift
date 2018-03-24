a98903a4483ea28838de67aa15ddb19a83c86463
HaijianLiu <haijanliu@gmail.com>
2018-03-24 17:55:43 +0900

    add Math.swift

---
+++ Shared/Math.swift
@@ -0,0 +1,32 @@
+// Copyright © haijian. All rights reserved.
+
+// Generic matrix math utility functions
+
+typealias Matrix4x4 = simd_float4x4
+typealias Vector3 = simd_float3
+typealias Vector4 = simd_float4
+
+func matrix4x4_rotation(radians: Float, axis: Vector3) -> Matrix4x4 {
+	let unitAxis = normalize(axis)
+	let ct = cosf(radians)
+	let st = sinf(radians)
+	let ci = 1 - ct
+	let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
+	return Matrix4x4.init(columns:(Vector4(ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0), Vector4(x * y * ci - z * st, ct + y * y * ci, z * y * ci + x * st, 0), Vector4(x * z * ci + y * st, y * z * ci - x * st, ct + z * z * ci, 0), Vector4(0, 0, 0, 1)))
+}
+
+func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> Matrix4x4 {
+	return Matrix4x4.init(columns:(Vector4(1, 0, 0, 0), Vector4(0, 1, 0, 0), Vector4(0, 0, 1, 0), Vector4(translationX, translationY, translationZ, 1)))
+}
+
+func matrix_perspective_right_hand(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> Matrix4x4 {
+	let ys = 1 / tanf(fovy * 0.5)
+	let xs = ys / aspectRatio
+	let zs = farZ / (nearZ - farZ)
+	return Matrix4x4.init(columns:(Vector4(xs,0, 0, 0), Vector4( 0, ys, 0, 0), Vector4( 0,  0, zs, -1), Vector4( 0, 0, zs * nearZ, 0)))
+}
+
+func radians_from_degrees(_ degrees: Float) -> Float {
+	return (degrees / 180) * .pi
+}
+
--- Shared/Renderer.swift
+++ Shared/Renderer.swift
@@ -4,7 +4,6 @@
 
 import Metal
 import MetalKit
-import simd
 
 // The 256 byte aligned size of our uniform structure
 let alignedUniformsSize = (MemoryLayout<Uniforms>.size & ~0xFF) + 0x100
@@ -32,7 +31,7 @@ class Renderer: NSObject, MTKViewDelegate {
 
 	var uniforms: UnsafeMutablePointer<Uniforms>
 
-	var projectionMatrix: matrix_float4x4 = matrix_float4x4()
+	var projectionMatrix: Matrix4x4 = matrix_float4x4()
 
 	var rotation: Float = 0
 
@@ -270,27 +269,3 @@ class Renderer: NSObject, MTKViewDelegate {
 	}
 }
 
-// Generic matrix math utility functions
-func matrix4x4_rotation(radians: Float, axis: float3) -> matrix_float4x4 {
-	let unitAxis = normalize(axis)
-	let ct = cosf(radians)
-	let st = sinf(radians)
-	let ci = 1 - ct
-	let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
-	return matrix_float4x4.init(columns:(vector_float4(ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0), vector_float4(x * y * ci - z * st, ct + y * y * ci, z * y * ci + x * st, 0), vector_float4(x * z * ci + y * st, y * z * ci - x * st, ct + z * z * ci, 0), vector_float4(0, 0, 0, 1)))
-}
-
-func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> matrix_float4x4 {
-	return matrix_float4x4.init(columns:(vector_float4(1, 0, 0, 0), vector_float4(0, 1, 0, 0), vector_float4(0, 0, 1, 0), vector_float4(translationX, translationY, translationZ, 1)))
-}
-
-func matrix_perspective_right_hand(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> matrix_float4x4 {
-	let ys = 1 / tanf(fovy * 0.5)
-	let xs = ys / aspectRatio
-	let zs = farZ / (nearZ - farZ)
-	return matrix_float4x4.init(columns:(vector_float4(xs,0, 0, 0), vector_float4( 0, ys, 0, 0), vector_float4( 0,  0, zs, -1), vector_float4( 0, 0, zs * nearZ, 0)))
-}
-
-func radians_from_degrees(_ degrees: Float) -> Float {
-	return (degrees / 180) * .pi
-}
