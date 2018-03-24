84e4bb27c277998ef0b8f5f39162c64b3e041b0b
HaijianLiu <haijanliu@gmail.com>
2018-03-24 18:47:11 +0900

    rebuild math functions, add let pi, change to public static

--- Shared/Math.swift
+++ Shared/Math.swift
@@ -7,7 +7,13 @@ typealias Vector4 = simd_float4
 typealias Matrix4x4 = simd_float4x4
 
 class Math {
-	static func matrix4x4_rotation(radians: Float, axis: Vector3) -> Matrix4x4 {
+
+	/// The mathematical constant pi.
+	/// pi = 3.14159265358979
+	public static let pi = Float.pi;
+
+	/// Builds a rotation 4 * 4 matrix created from an axis of 3 scalars and an angle expressed in radians.
+	public static func rotate(radians: Float, axis: Vector3) -> Matrix4x4 {
 		let unitAxis = normalize(axis)
 		let ct = cosf(radians)
 		let st = sinf(radians)
@@ -15,20 +21,23 @@ class Math {
 		let x = unitAxis.x, y = unitAxis.y, z = unitAxis.z
 		return Matrix4x4.init(columns:(Vector4(ct + x * x * ci, y * x * ci + z * st, z * x * ci - y * st, 0), Vector4(x * y * ci - z * st, ct + y * y * ci, z * y * ci + x * st, 0), Vector4(x * z * ci + y * st, y * z * ci - x * st, ct + z * z * ci, 0), Vector4(0, 0, 0, 1)))
 	}
-	
-	static func matrix4x4_translation(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> Matrix4x4 {
+
+	/// Builds a translation 4 * 4 matrix created from 3 scalars.
+	public static func translate(_ translationX: Float, _ translationY: Float, _ translationZ: Float) -> Matrix4x4 {
 		return Matrix4x4.init(columns:(Vector4(1, 0, 0, 0), Vector4(0, 1, 0, 0), Vector4(0, 0, 1, 0), Vector4(translationX, translationY, translationZ, 1)))
 	}
-	
-	static func matrix_perspective_right_hand(fovyRadians fovy: Float, aspectRatio: Float, nearZ: Float, farZ: Float) -> Matrix4x4 {
+
+	/// Creates a matrix for a symetric perspective-view frustum. (Right hand)
+	public static func perspective(fovyRadians fovy: Float, aspect: Float, near: Float, far: Float) -> Matrix4x4 {
 		let ys = 1 / tanf(fovy * 0.5)
-		let xs = ys / aspectRatio
-		let zs = farZ / (nearZ - farZ)
-		return Matrix4x4.init(columns:(Vector4(xs,0, 0, 0), Vector4( 0, ys, 0, 0), Vector4( 0,  0, zs, -1), Vector4( 0, 0, zs * nearZ, 0)))
+		let xs = ys / aspect
+		let zs = far / (near - far)
+		return Matrix4x4.init(columns:(Vector4(xs,0, 0, 0), Vector4( 0, ys, 0, 0), Vector4( 0,  0, zs, -1), Vector4( 0, 0, zs * near, 0)))
 	}
-	
-	static func radians_from_degrees(_ degrees: Float) -> Float {
-		return (degrees / 180) * .pi
+
+	/// Converts degrees to radians and returns the result.
+	public static func radians(_ degrees: Float) -> Float {
+		return (degrees / 180) * self.pi
 	}
 }
 
