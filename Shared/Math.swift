caaef91d980e0237c90ccdd68deb955923cfa072
HaijianLiu <haijanliu@gmail.com>
2018-03-24 18:51:44 +0900

    syntax

--- Shared/Math.swift
+++ Shared/Math.swift
@@ -7,7 +7,6 @@ typealias Vector4 = simd_float4
 typealias Matrix4x4 = simd_float4x4
 
 class Math {
-
 	/// The mathematical constant pi.
 	/// pi = 3.14159265358979
 	public static let pi = Float.pi;
