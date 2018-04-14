61454aebab2ae462dd8a8c2b0e4a8348de65b06a
HaijianLiu <haijanliu@gmail.com>
2018-04-14 17:42:20 +0900

    add Mesh and MTLRenderPipelineState to MeshRenderer

--- Phantom/MeshRenderer.swift
+++ Phantom/MeshRenderer.swift
@@ -1,9 +1,28 @@
 // Copyright © haijian. All rights reserved.
 
+import MetalKit
+
 class MeshRenderer: Component {
 	var gameObject: GameObject?
-	
 	var transform: Transform?
 	
-	required init() {}
+	var pipelineState: MTLRenderPipelineState?
+	
+	var mesh: Mesh? {
+		didSet {
+			// pipeline state
+			do {
+				pipelineState = try Renderer.buildRenderPipelineWithDevice(device: Display.main.device!, metalKitView: Display.main, mtlVertexDescriptor: (mesh?.mtlVertexDescriptor)!)
+			} catch {
+				print("Unable to compile render pipeline state.  Error info: \(error)")
+				mesh = nil
+			}
+		}
+	}
+	
+	var texture: Texture?
+	
+	required init() {
+		
+	}
 }
