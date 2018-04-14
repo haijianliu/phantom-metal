540833c2ba4a4e16fb28bb205c999e8ad3bf9137
HaijianLiu <haijanliu@gmail.com>
2018-04-14 17:39:22 +0900

    Application init GameObject

--- Phantom/Application.swift
+++ Phantom/Application.swift
@@ -3,6 +3,9 @@
 class Application {
 	
 	var renderer: Renderer?
+	
+	var gameObjects = [GameObject]()
+	
 
 	func launch() {
 		// Create Renderer
@@ -12,11 +15,36 @@ class Application {
 		}
 		renderer = newRenderer
 		
+
+		// GameObject
+		let gameObject = GameObject()
+		// MeshRenderer
+		let meshRenderer: MeshRenderer? = gameObject.addComponent()
+		// Attach Mesh
+		guard let mesh = Mesh() else { return }
+		meshRenderer?.mesh = mesh
+		// Attach Texture
+		// Texture
+		let texture = Texture()
+		do {
+			texture.mtlTexture = try Texture.load(textureName: "UV_Grid_Sm")
+		} catch {
+			print("Unable to load texture. Error info: \(error)")
+			return
+		}
+		meshRenderer?.texture = texture
+		// Add GameObject
+		gameObjects.append(gameObject)
+		
+		renderer?.gameObject = gameObject
+		
 		// TODO: This will be a Display process
 		// Set MTKViewDelegate to current Renderer instance
 		Display.main.delegate = renderer
-
+		
 		// Only for the first time, should initiate the view manually
 		renderer?.mtkView(Display.main, drawableSizeWillChange: Display.main.drawableSize)
+		
+		
 	}
 }
