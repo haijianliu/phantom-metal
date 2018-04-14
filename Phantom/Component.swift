d71bd69b706d547b4b9e7d801f7ce292d674d995
HaijianLiu <haijanliu@gmail.com>
2018-04-14 15:39:12 +0900

    add getComponet and addComponent functions

---
+++ Phantom/Component.swift
@@ -0,0 +1,14 @@
+// Copyright © haijian. All rights reserved.
+
+/// Protocol for everything attached to GameObject(s).
+protocol Component {
+	
+	/// The game object this component is attached to.
+	/// A component is always attached to a game object.
+	var gameObject: GameObject { get }
+	
+	/// The Transform attached to this GameObject.
+	var transform: Transform { get }
+	
+	init()
+}
--- Phantom/GameObject.swift
+++ Phantom/GameObject.swift
@@ -5,4 +5,26 @@ import MetalKit
 class GameObject {
 	var transform = Transform()
 	
+	private var components = [String: Component]()
+	
+	/// Adds a component class named type name to the game object.
+	///
+	/// If there is already a same type of componet added, this function will do nothing, and return a nil
+	/// - Returns: Componet instance if succeed, otherwise nil
+	func addComponent<T: Component>() -> T? {
+		let typeName = String(describing: T.self)
+		if components[typeName] == nil {
+			components[typeName] = T()
+			return components[typeName] as? T
+		} else {
+			return nil
+		}
+	}
+	
+	/// Get a component instance attached to the game object by component type.
+	/// - Returns: Component instance of component type if the game object has one attached, nil if it doesn't.
+	func getComponent<T: Component>() -> T? {
+		return components[String(describing: T.self)] as? T
+	}
 }
+
