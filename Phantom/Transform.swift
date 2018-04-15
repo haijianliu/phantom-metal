3f43c1f39543cf92a5f6e9f88a842fdecabd21d7
HaijianLiu <haijanliu@gmail.com>
2018-04-15 23:46:15 +0900

    Transform is Component

--- Phantom/Transform.swift
+++ Phantom/Transform.swift
@@ -1,55 +1,20 @@
 // Copyright © haijian. All rights reserved.
 
-import MetalKit
-
-// Function(vertexShader): the offset into the buffer uniforms that is bound at buffer index must be a multiple of 256
-let alignedUniformsSize = (MemoryLayout<Uniforms>.size & ~0xFF) + 0x100
-let maxBuffersInFlight = 3
-
-class Transform {
+class Transform: Component {
 	
-	var uniforms: UnsafeMutablePointer<Uniforms>
 	var projectionMatrix: Matrix4x4 = Matrix4x4()
 	var rotation: Float = 0
 	
-	var dynamicUniformBuffer: MTLBuffer
-	
-	let inFlightSemaphore = DispatchSemaphore(value: maxBuffersInFlight)
-	var uniformBufferOffset = 0
-	var uniformBufferIndex = 0
-	
-	init?() {
-		let uniformBufferSize = alignedUniformsSize * maxBuffersInFlight
-		
-		guard let buffer = Display.main.device?.makeBuffer(length: uniformBufferSize, options: MTLResourceOptions.storageModeShared) else { return nil }
-		dynamicUniformBuffer = buffer
-		
-		self.dynamicUniformBuffer.label = "UniformBuffer"
-		
-		uniforms = UnsafeMutableRawPointer(dynamicUniformBuffer.contents()).bindMemory(to: Uniforms.self, capacity: 1)
-	}
-	
-	
 	func update() {
-		// Update any game state before rendering
 		
-		updateDynamicBufferState()
+		gameObject.updateDynamicBufferState()
 		
-		uniforms[0].projectionMatrix = projectionMatrix
+		gameObject.uniforms[0].projectionMatrix = projectionMatrix
 		
 		let rotationAxis = float3(1, 1, 0)
 		let modelMatrix = Math.rotate(radians: rotation, axis: rotationAxis)
 		let viewMatrix = Math.translate(0.0, 0.0, -8.0)
-		uniforms[0].modelViewMatrix = viewMatrix * modelMatrix;
+		gameObject.uniforms[0].modelViewMatrix = viewMatrix * modelMatrix;
 		rotation += 0.01
-	}
-	
-	private func updateDynamicBufferState() {
-		/// Update the state of our uniform buffers before rendering
-		uniformBufferIndex = (uniformBufferIndex + 1) % maxBuffersInFlight
-		
-		uniformBufferOffset = alignedUniformsSize * uniformBufferIndex
-		
-		uniforms = UnsafeMutableRawPointer(dynamicUniformBuffer.contents() + uniformBufferOffset).bindMemory(to:Uniforms.self, capacity:1)
 	}
 }
