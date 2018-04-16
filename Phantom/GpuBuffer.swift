aa7643f4673d0914fcaa6920a420b28da79e5602
HaijianLiu <haijanliu@gmail.com>
2018-04-17 00:13:31 +0900

    change descriptions

--- Phantom/GpuBuffer.swift
+++ Phantom/GpuBuffer.swift
@@ -2,29 +2,29 @@
 
 import MetalKit
 
-/// Buffer needs to pass to shaders as uniforms
+/// Buffer needs to pass to shaders as uniforms.
 ///
-/// When update, requires semaphore available
-/// TODO: adopt Uniform protocol
+/// When update, requires semaphore available.
+/// TODO: adopt Uniform protocol.
 struct GpuBuffer<BufferType> {
-	// Function(vertexShader): the offset into the buffer uniforms that is bound at buffer index must be a multiple of 256
+	// Function(vertexShader): the offset into the buffer uniforms that is bound at buffer index must be a multiple of 256.
 	let alignedSize = (MemoryLayout<BufferType>.size & ~0xFF) + 0x100
-	/// The number of copies of the buffer
+	/// The number of copies of the buffer.
 	let max: Int
-	/// Current data offer in bytes
+	/// Current data offer in bytes.
 	var offset = 0
-	/// Current data index
+	/// Current data index.
 	var index = 0
 	
 	let semaphore: DispatchSemaphore
 	var buffer: MTLBuffer
 	var pointer: UnsafeMutablePointer<BufferType>
 	
-	/// Create a GpuBuffer with semaphore vaule
+	/// Create a GpuBuffer with semaphore vaule.
 	///
 	/// - Parameters:
-	///   - semaphoreValue: members of threads can update this buffer by at the same time
-	///   - options: metal resource options
+	///   - semaphoreValue: members of threads can update this buffer by at the same time.
+	///   - options: metal resource options.
 	init?(semaphoreValue: Int, options: MTLResourceOptions) {
 		max = semaphoreValue
 		semaphore = DispatchSemaphore(value: semaphoreValue)
@@ -35,6 +35,7 @@ struct GpuBuffer<BufferType> {
 		pointer = UnsafeMutableRawPointer(buffer.contents()).bindMemory(to: BufferType.self, capacity: 1)
 	}
 
+	// TODO: private
 	mutating func updateBufferState() {
 		/// Update the state of our uniform buffers before rendering
 		index = (index + 1) % max
