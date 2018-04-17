c7190c92bd2bd2b5710a71c8710071680062511f
HaijianLiu <haijanliu@gmail.com>
2018-04-18 01:02:48 +0900

    change include path

---
+++ Phantom/Phantom.h
@@ -0,0 +1,13 @@
+// Copyright © haijian. All rights reserved.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Phantom.
+FOUNDATION_EXPORT double PhantomVersionNumber;
+
+//! Project version string for Phantom.
+FOUNDATION_EXPORT const unsigned char PhantomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Phantom/PublicHeader.h>
+
+
--- Phantom/Shaders/Shaders.metal
+++ Phantom/Shaders/Shaders.metal
@@ -5,7 +5,7 @@
 #include <metal_stdlib>
 
 // Including header shared between this Metal shader code and Swift/C code executing Metal API commands
-#import "Phantom/Bridges/ShaderTypes.h"
+#import "BridgingHeaders/ShaderTypes.h"
 
 using namespace metal;
 
