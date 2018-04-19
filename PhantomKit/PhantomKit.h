b9aa8eff113db2b009bd8d7cf7f69e6d1adefb22
HaijianLiu <haijanliu@gmail.com>
2018-04-20 01:59:54 +0900

    create framework target

---
+++ PhantomKit/PhantomKit.h
@@ -0,0 +1,11 @@
+// Copyright © haijian. All rights reserved.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PhantomKit.
+FOUNDATION_EXPORT double PhantomKitVersionNumber;
+
+//! Project version string for PhantomKit.
+FOUNDATION_EXPORT const unsigned char PhantomKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhantomKit/PublicHeader.h>
