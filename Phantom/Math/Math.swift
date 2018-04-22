eef0e79ce3b584087c9221a300a2cbacc7803733
HaijianLiu <haijanliu@gmail.com>
2018-04-22 12:44:16 +0900

    add new type

--- Phantom/Math/Math.swift
+++ Phantom/Math/Math.swift
@@ -4,6 +4,11 @@
 public typealias Vector3 = simd_float3
 public typealias Vector4 = simd_float4
 public typealias Matrix4x4 = simd_float4x4
+public typealias Quaternion = simd_quatf
+
+// TODO: constrain
+public typealias Radian = Float
+public typealias Degree = Float
 
 /// Math base class
 public class Math {}
