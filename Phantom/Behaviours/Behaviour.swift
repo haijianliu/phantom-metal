e5340fd11bd37eda3a0096cf4743a243190091a1
HaijianLiu <haijanliu@gmail.com>
2018-04-25 23:33:53 +0900

    Behaviour now is a objective c procotol and constrainted by Component class

--- Phantom/Behaviours/Behaviour.swift
+++ Phantom/Behaviours/Behaviour.swift
@@ -1,11 +1,10 @@
 // Copyright © haijian. All rights reserved.
 
 /// Behaviours are Components that can be enabled or disabled.
-open class Behaviour: Component {
+@objc public protocol Behaviour where Self: Component {
 	
 	/// Enabled Behaviours are updated, disabled Behaviours are not.
-	// TODO: setter getter to update behaviour-list
-	public var enabled: Bool {
-		return true
-	}
+	///
+	/// Automatically adopted and comformed by Component (Default: true).
+	var enabled: Bool { get set }
 }
