471a6e9a4da05e84d3852e9bef19ca61e3378a82
HaijianLiu <haijanliu@gmail.com>
2018-04-25 23:34:40 +0900

    just comforms Behaviour protocol

--- Phantom/Behaviours/Updatable.swift
+++ Phantom/Behaviours/Updatable.swift
@@ -3,12 +3,11 @@
 /// Updatable Behaviour protocol updates Behavour every frame.
 ///
 /// Requires that class inherits from Behaviour
-@objc public protocol Updatable where Self: Behaviour {
+@objc public protocol Updatable: Behaviour {
 	/// Update is called every frame, if the Behaviour is enabled.
 	///
 	/// In order to get the elapsed time since last call to Update, use Time.deltaTime. This function is only called if the Behaviour is enabled. Override this function in order to provide your component's functionality.
 	func update()
 }
 
-// TODO: Remove "redundant layout constraint" warning [SR-6265](https://bugs.swift.org/browse/SR-6265)
-// public protocol Updatable: AnyObject where Self: Behaviour
+// TODO: "redundant layout constraint" warning [SR-6265](https://bugs.swift.org/browse/SR-6265)
