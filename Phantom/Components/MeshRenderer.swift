f81e2aa782ade17057e8d29b463e4086678f0c31
HaijianLiu <haijanliu@gmail.com>
2018-04-29 02:12:14 +0900

    implement draw function

--- Phantom/Components/MeshRenderer.swift
+++ Phantom/Components/MeshRenderer.swift
@@ -4,13 +4,13 @@ import MetalKit
 
 public class MeshRenderer: Renderer, Drawable {
 	
-	var pipelineState: MTLRenderPipelineState?
+	var renderPipelineState: MTLRenderPipelineState?
 	
 	public var mesh: Mesh? {
 		didSet {
 			// pipeline state
 			do {
-				pipelineState = try ViewDelegate.buildRenderPipelineWithDevice(device: View.main.device!, metalKitView: View.main, mtlVertexDescriptor: (mesh?.mtlVertexDescriptor)!)
+				renderPipelineState = try ViewDelegate.buildRenderPipelineWithDevice(device: View.main.device!, metalKitView: View.main, mtlVertexDescriptor: (mesh?.mtlVertexDescriptor)!)
 			} catch {
 				print("Unable to compile render pipeline state.  Error info: \(error)")
 				mesh = nil
@@ -20,6 +20,70 @@ public class MeshRenderer: Renderer, Drawable {
 	
 	public var texture: Texture? // TODO: Material
 	
-	func draw() {
+	func draw(in view: MTKView) {
+		
+		// TODO: wait in game object
+		let semaphore = gameObject.getSemaphore()
+		_ = semaphore.wait(timeout: .distantFuture)
+				
+		if let commandBuffer = View.sharedInstance.commandQueue?.makeCommandBuffer() {
+			
+			commandBuffer.addCompletedHandler() { _ in semaphore.signal() }
+			
+			gameObject.update()
+			
+			if let renderPassDescriptor = view.currentRenderPassDescriptor, let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) {
+				
+				/// Final pass rendering code here
+				renderEncoder.label = "Primary Render Encoder"
+				renderEncoder.pushDebugGroup("Draw Box")
+				
+				// TODO: in material
+				renderEncoder.setCullMode(.back)
+				renderEncoder.setFrontFacing(.counterClockwise)
+				
+				// TODO: in rendering pass
+				guard let depthStencilState = View.sharedInstance.renderPass?.depthStencilState else { return }
+				renderEncoder.setDepthStencilState(depthStencilState)
+				
+				// TODO: in material
+				// TODO: in mesh
+				guard let renderPipelineState = self.renderPipelineState else { return }
+				renderEncoder.setRenderPipelineState(renderPipelineState)
+
+				// TODO: in game object
+				// TODO: BufferIndex
+				renderEncoder.setVertexBuffer(gameObject.transformUniformBuffer.buffer, offset: gameObject.transformUniformBuffer.offset, index: BufferIndex.uniforms.rawValue)
+				
+				// TODO: in mesh
+				guard let mesh = self.mesh else { return }
+				for (index, element) in mesh.mtkMesh.vertexDescriptor.layouts.enumerated() {
+					guard let layout = element as? MDLVertexBufferLayout else { return }
+					if layout.stride != 0 {
+						let vertexBuffers = mesh.mtkMesh.vertexBuffers[index]
+						renderEncoder.setVertexBuffer(vertexBuffers.buffer, offset: vertexBuffers.offset, index: index)
+					}
+				}
+				
+				// TODO: in material
+				guard let texture = self.texture else { return }
+				renderEncoder.setFragmentTexture(texture.mtlTexture, index: TextureIndex.color.rawValue)
+				
+				// TODO: in mesh
+				for submesh in mesh.mtkMesh.submeshes {
+					renderEncoder.drawIndexedPrimitives(type: submesh.primitiveType, indexCount: submesh.indexCount, indexType: submesh.indexType, indexBuffer: submesh.indexBuffer.buffer, indexBufferOffset: submesh.indexBuffer.offset)
+				}
+				
+				renderEncoder.popDebugGroup()
+				
+				renderEncoder.endEncoding()
+				
+				if let drawable = view.currentDrawable {
+					commandBuffer.present(drawable)
+				}
+			}
+			
+			commandBuffer.commit()
+		}
 	}
 }
