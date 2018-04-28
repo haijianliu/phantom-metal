f6d084de97db14e1d61295cf591ffee3c0a804fd
HaijianLiu <haijanliu@gmail.com>
2018-04-29 02:06:22 +0900

    draw function recieve a metal view argument

--- Phantom/Behaviours/Drawable.swift
+++ Phantom/Behaviours/Drawable.swift
@@ -1,12 +1,14 @@
 // Copyright © haijian. All rights reserved.
 
+import MetalKit
+
 /// Drawable Behaviour protocol to draw comformed objects on the screen.
 ///
 /// Requires that class inherits from Renderer
 @objc protocol Drawable: Behaviour where Self: Renderer {
-	
+
 	/// Draw objects on the screen.
 	///
 	/// Updated every frame by render thread (s).
-	func draw()
+	func draw(in view: MTKView)
 }
