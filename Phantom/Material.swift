359bbdddef157c7547315ffcbead1c67758cbeaf
HaijianLiu <haijanliu@gmail.com>
2018-04-29 15:29:41 +0900

    material encode both shader and texture

--- Phantom/Material.swift
+++ Phantom/Material.swift
@@ -3,10 +3,21 @@
 import MetalKit
 
 // TODO: struct only protocol
+// TODO: when add drawable behaviour to list, check if there is shader and texture attached. If not, assign one.
 public struct Material {
+
 	var cullMode: MTLCullMode = MTLCullMode.back
 	
+	public var shader: Shader?
 	public var texture: Texture?
 	
 	public init() {}
+}
+
+extension Material: Encodable {
+	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
+		renderCommandEncoder.setCullMode(cullMode)
+		shader?.encode(to: renderCommandEncoder)
+		texture?.encode(to: renderCommandEncoder)
+	}
 }
