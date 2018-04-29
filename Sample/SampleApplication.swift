ef497cd8445d2b9449a05343a0882a75888680b7
HaijianLiu <haijanliu@gmail.com>
2018-04-29 15:31:28 +0900

    implement

--- Sample/SampleApplication.swift
+++ Sample/SampleApplication.swift
@@ -13,14 +13,17 @@ class SampleApplication: ApplicationDelegate {
 		guard let gameObject = GameObject() else { return }
 		// MeshRenderer
 		guard let meshRenderer: MeshRenderer = gameObject.addComponent() else { return }
-		// Attach Mesh
-		guard let mesh = Mesh() else { return }
-		meshRenderer.mesh = mesh
 		// Attach material
 		var material = Material()
+		guard let shader = Shader() else { return }
 		guard let texture = Texture(name: "UV_Grid_Sm") else { return }
 		material.texture = texture
+		material.shader = shader
 		meshRenderer.material = material
+		// Attach Mesh.
+		// TODO: automatically link mesh and vertex descriptor from shader when attach mesh to mesh renderer.
+		guard let mesh = Mesh(shader: shader) else { return }
+		meshRenderer.mesh = mesh
 		// SampleBehaviour
 		guard let _: SampleBehaviour = gameObject.addComponent() else { return }
 		// Add GameObject
