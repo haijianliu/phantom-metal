e8f8dae1a17ac4466ca4edd2df5a42e51dc3ddf5
HaijianLiu <haijanliu@gmail.com>
2018-04-29 13:26:46 +0900

    add culling mode property and texture slot

--- Phantom/Material.swift
+++ Phantom/Material.swift
@@ -1,6 +1,12 @@
 // Copyright © haijian. All rights reserved.
 
+import MetalKit
+
 // TODO: struct only protocol
 public struct Material {
+	var cullMode: MTLCullMode = MTLCullMode.back
 	
+	public var texture: Texture?
+	
+	public init() {}
 }
