73d8908229b28a0c7ae272c4495d9f6406f25a84
HaijianLiu <haijanliu@gmail.com>
2018-05-19 12:57:09 +0900

    Temporarily put some render pass encoding code here.

--- Phantom/Framework/ViewDelegate.swift
+++ Phantom/Framework/ViewDelegate.swift
@@ -7,18 +7,46 @@ import MetalKit
 
 class ViewDelegate: NSObject, MTKViewDelegate {
 
+	// TODO: only render render pass here.
 	func draw(in view: MTKView) {
+		
+		// TODO: in render pass.
+		guard
+			let renderPass = View.sharedInstance.renderPass,
+			let commandBuffer = View.sharedInstance.commandQueue?.makeCommandBuffer(),
+			let renderEncoder = renderPass.makeRenderCommandEncoder(commandBuffer: commandBuffer)
+		else { return }
+		
 		// update behaviours
 		// TODO: multi-thread update
 		for updateBehaviour in Application.sharedInstance.updateBehaviours {
 			updateBehaviour.reference?.update()
 		}
 		
+		// TODO: semaphore.
+//		_ = semaphore.wait(timeout: .distantFuture)
+//		commandBuffer.addCompletedHandler() { _ in self.semaphore.signal() } // TODO: capture
+		
+		// Start encoding and setup debug infomation
+		// TODO: setup with render pass names
+		renderEncoder.label = "Primary Render Encoder"
+		// Render pass encoding.
+		renderPass.encode(to: renderEncoder)
+		
 		// drawable behaviours
 		// TODO: multiple threads draw multiple queue (realtime and offline rendering)
 		for drawBehaviour in Application.sharedInstance.drawBehaviours {
-			drawBehaviour.reference?.draw(in: view)
+			drawBehaviour.reference?.draw(encoding: renderEncoder)
 		}
+		
+		// End encoding.
+		renderEncoder.endEncoding()
+		
+		// If rendering to core animation layer.
+		// TODO: in render pass
+		if let drawable = view.currentDrawable { commandBuffer.present(drawable) }
+		
+		commandBuffer.commit()
 	}
 
 	func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
