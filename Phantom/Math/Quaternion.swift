76d2cddbb74ef17ae77b163b935bbc47b424c02f
HaijianLiu <haijanliu@gmail.com>
2018-05-19 14:46:37 +0900

    Chore.

--- Phantom/Math/Quaternion.swift
+++ Phantom/Math/Quaternion.swift
@@ -1,12 +1,13 @@
 // Copyright © haijian. All rights reserved.
 
 extension Math {
+	
+	// TODO: test
 	/// Build a quaternion from an angle and a normalized axis.
 	///
 	/// - Parameters:
 	///   - angle: Angle expressed in radians.
 	///   - axis: Axis of the quaternion, must be normalized.
-	// TODO: test
 	public static func angleAxis(_ angle: Radian, _ axis: Vector3) -> Quaternion {
 		return simd_quatf(angle: angle, axis: axis)
 	}
