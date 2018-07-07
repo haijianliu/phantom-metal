d7ab671a3f0bf485b682345192716f326464bb76
HaijianLiu <haijanliu@gmail.com>
2018-07-07 12:59:01 +0900

    Add create cube function.

--- Phantom/GameObject/GameObjectPrimitive.swift
+++ Phantom/GameObject/GameObjectPrimitive.swift
@@ -1,5 +1,7 @@
 // Copyright © haijian. All rights reserved.
 
+import MetalKit
+
 // MARK: - Primitive extension for creating various primitive gameobjects by using the GameObject.create... functions.
 extension GameObject {
 
@@ -24,12 +26,30 @@ extension GameObject {
 	
 	/// Create a cube primitive.
 
-	public static func createCube() -> GameObject? {
+	public static func createCube(withDimensions dimensions: Vector3 = Vector3(1, 1, 1), segments: Uint3 = Uint3(1, 1, 1), geometryType: MDLGeometryType = .triangles, inwardNormals: Bool = false) -> GameObject? {
 		guard let gameObject = GameObject.createBase() else { return nil }
 		guard let meshRenderer: MeshRenderer = gameObject.getComponent() else { return nil }
+		guard let vertexDescriptor = meshRenderer.material?.shader?.vertexDescriptor else { return nil }
+
 		// TODO: refactor!
-		guard let mesh = Mesh(shader: (meshRenderer.material?.shader)!) else { return nil }
-		meshRenderer.mesh = mesh
+		guard let device = View.main.device else { return nil }
+		let metalAllocator = MTKMeshBufferAllocator(device: device)
+		let mdlMesh = MDLMesh.newBox(withDimensions: dimensions, segments: segments, geometryType: geometryType, inwardNormals: inwardNormals, allocator: metalAllocator)
+		let mdlVertexDescriptor = MTKModelIOVertexDescriptorFromMetal(vertexDescriptor)
+		guard let attributes = mdlVertexDescriptor.attributes as? [MDLVertexAttribute] else { return nil }
+		attributes[VertexAttribute.position.rawValue].name = MDLVertexAttributePosition
+		attributes[VertexAttribute.texcoord.rawValue].name = MDLVertexAttributeTextureCoordinate
+		mdlMesh.vertexDescriptor = mdlVertexDescriptor
+		let mtkMesh: MTKMesh
+		do {
+			mtkMesh = try MTKMesh(mesh: mdlMesh, device: device)
+		} catch {
+			print("Unable to build MetalKit Mesh. Error info: \(error)")
+			return nil
+		}
+		
+		meshRenderer.mesh = Mesh(with: mtkMesh)
+
 		return gameObject
 	}
 }
