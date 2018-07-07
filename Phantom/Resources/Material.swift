3ec88ee18eff34e191146570c454dc5238a46ef1
HaijianLiu <haijanliu@gmail.com>
2018-07-07 13:38:10 +0900

    Change initializer.

--- Phantom/Resources/Material.swift
+++ Phantom/Resources/Material.swift
@@ -5,19 +5,17 @@ import MetalKit
 // TODO: struct only protocol
 // TODO: when add drawable behaviour to list, check if there is shader and texture attached. If not, assign one.
 public class Material {
-
-	var cullMode: MTLCullMode = MTLCullMode.back
-	
-	public var shader: Shader?
+	public var shader: Shader
 	public var texture: Texture?
-	
-	public init() {}
+	var cullMode: MTLCullMode = MTLCullMode.back
+
+	public init(with shader: Shader) { self.shader = shader }
 }
 
 extension Material: Encodable {
 	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
 		renderCommandEncoder.setCullMode(cullMode)
-		shader?.encode(to: renderCommandEncoder)
+		shader.encode(to: renderCommandEncoder)
 		texture?.encode(to: renderCommandEncoder)
 	}
 }
