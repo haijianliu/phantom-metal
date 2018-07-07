739e392c18a30305f87d6e0a0fa958cb322d561e
HaijianLiu <haijanliu@gmail.com>
2018-07-07 12:53:43 +0900

    Add typealias Uint3 = simd_uint3

--- Phantom/Math/Math.swift
+++ Phantom/Math/Math.swift
@@ -5,6 +5,7 @@ public typealias Vector3 = simd_float3
 public typealias Vector4 = simd_float4
 public typealias Matrix4x4 = simd_float4x4
 public typealias Quaternion = simd_quatf
+public typealias Uint3 = simd_uint3
 
 // TODO: constrain
 public typealias Radian = Float
