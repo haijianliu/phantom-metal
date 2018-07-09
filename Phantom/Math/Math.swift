4f9a9280cb8d8e2c6bccbc5ea9aec38f66db4110
HaijianLiu <haijanliu@gmail.com>
2018-07-10 01:14:37 +0900

    Add new typealias: Vector2, Uint2.

--- Phantom/Math/Math.swift
+++ Phantom/Math/Math.swift
@@ -1,10 +1,12 @@
 // Copyright © haijian. All rights reserved.
 
 // Math type alias from simd types
+public typealias Vector2 = simd_float2
 public typealias Vector3 = simd_float3
 public typealias Vector4 = simd_float4
 public typealias Matrix4x4 = simd_float4x4
 public typealias Quaternion = simd_quatf
+public typealias Uint2 = simd_uint2
 public typealias Uint3 = simd_uint3
 
 // TODO: constrain
