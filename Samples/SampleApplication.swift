3e366bf86eef3ae865759702ec314afa97475401
HaijianLiu <haijanliu@gmail.com>
2018-07-10 01:16:03 +0900

    Add a plane primitive sample.

--- Samples/SampleApplication.swift
+++ Samples/SampleApplication.swift
@@ -4,32 +4,43 @@ import PhantomKit
 
 class SampleApplication: ApplicationDelegate {
 	func start() {
-		createGameObjects(position: Vector3(x: -3, y: 0, z: 0))
-		createGameObjects(position: Vector3(x:  3, y: 0, z: 0))
-		createMainCamera()
-	}
-	
-	private func createGameObjects(position: Vector3) {
-		// GameObject
-		guard let gameObject = GameObject.createCube(withDimensions: Vector3(2, 2, 2)) else { return }
-		// Set texture
-		gameObject.material?.texture = Texture(name: "UV_Grid_Sm")
-		// Transform
-		gameObject.transform.position = position
-		// SampleBehaviour
-		guard let _: SampleBehaviour = gameObject.addComponent() else { return }
-		// Add GameObject
-		Application.addGameObject(gameObject)
-	}
-	
-	private func createMainCamera() {
-		// GameObject
-		guard let gameObject = GameObject() else { return }
-		// Camera
-		guard let _: Camera = gameObject.addComponent() else { return }
-		// Transform.
-		gameObject.transform.position.z = -8.0;
-		// Add GameObject
-		Application.addGameObject(gameObject)
+
+		// Create cube.
+		do {
+			// GameObject
+			guard let gameObject = GameObject.createCube(withDimensions: Vector3(2, 2, 2)) else { return }
+			// Transform
+			gameObject.transform.position = Vector3(x: -3, y: 0, z: 0)
+			// SampleBehaviour
+			guard let _: SampleBehaviour = gameObject.addComponent() else { return }
+			// Add GameObject
+			Application.addGameObject(gameObject)
+		}
+
+		// Create plane.
+		do {
+			// GameObject
+			guard let gameObject = GameObject.createPlane() else { return }
+			// Set texture
+			gameObject.material?.texture = Texture(name: "UV_Grid_Sm")
+			// Transform
+			gameObject.transform.position = Vector3(x: 3, y: 0, z: 0)
+			// SampleBehaviour
+			guard let _: SampleBehaviour = gameObject.addComponent() else { return }
+			// Add GameObject
+			Application.addGameObject(gameObject)
+		}
+
+		// Create camera.
+		do {
+			// GameObject
+			guard let gameObject = GameObject() else { return }
+			// Camera
+			guard let _: Camera = gameObject.addComponent() else { return }
+			// Transform.
+			gameObject.transform.position.z = -8.0;
+			// Add GameObject
+			Application.addGameObject(gameObject)
+		}
 	}
 }
