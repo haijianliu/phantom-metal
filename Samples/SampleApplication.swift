f391adbfd72edf6befca23f4ad4404cdf60cb45b
HaijianLiu <haijanliu@gmail.com>
2018-07-10 17:40:51 +0900

    add .

--- Samples/SampleApplication.swift
+++ Samples/SampleApplication.swift
@@ -6,37 +6,37 @@ class SampleApplication: ApplicationDelegate {
 	func start() {
 
 		// Create cube.
-		do {
+		cube: do {
 			// GameObject
-			guard let gameObject = GameObject.createCube(withDimensions: Vector3(2, 2, 2)) else { return }
+			guard let gameObject = GameObject.createCube(withDimensions: Vector3(2, 2, 2)) else { break cube }
 			// Transform
 			gameObject.transform.position = Vector3(x: -3, y: 0, z: 0)
 			// SampleBehaviour
-			guard let _: SampleBehaviour = gameObject.addComponent() else { return }
+			guard let _: SampleBehaviour = gameObject.addComponent() else { break cube }
 			// Add GameObject
 			Application.addGameObject(gameObject)
 		}
 
 		// Create plane.
-		do {
+		plane: do {
 			// GameObject
-			guard let gameObject = GameObject.createPlane() else { return }
+			guard let gameObject = GameObject.createPlane() else { break plane }
 			// Set texture
 			gameObject.material?.texture = Texture(name: "UV_Grid_Sm")
 			// Transform
 			gameObject.transform.position = Vector3(x: 3, y: 0, z: 0)
 			// SampleBehaviour
-			guard let _: SampleBehaviour = gameObject.addComponent() else { return }
+			guard let _: SampleBehaviour = gameObject.addComponent() else { break plane }
 			// Add GameObject
 			Application.addGameObject(gameObject)
 		}
 
 		// Create camera.
-		do {
+		camera: do {
 			// GameObject
-			guard let gameObject = GameObject() else { return }
+			guard let gameObject = GameObject() else { break camera }
 			// Camera
-			guard let _: Camera = gameObject.addComponent() else { return }
+			guard let _: Camera = gameObject.addComponent() else { break camera }
 			// Transform.
 			gameObject.transform.position.z = -8.0;
 			// Add GameObject
