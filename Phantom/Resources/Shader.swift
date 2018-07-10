fc3debac564efcfca9883b7033f5417534f07419
Adam Nemecek <adamnemecek@gmail.com>
2018-07-10 08:14:56 -0700

    refactoring in shader

--- Phantom/Resources/Shader.swift
+++ Phantom/Resources/Shader.swift
@@ -52,21 +52,21 @@ extension Shader {
 		// Creete a Metal vertex descriptor specifying how vertices will by laid out for input into our render pipeline and how we'll layout our Model IO vertices
 		let vertexDescriptor = MTLVertexDescriptor()
 
-		vertexDescriptor.attributes[VertexAttribute.position.rawValue].format = MTLVertexFormat.float3
+		vertexDescriptor.attributes[VertexAttribute.position.rawValue].format = .float3
 		vertexDescriptor.attributes[VertexAttribute.position.rawValue].offset = 0
 		vertexDescriptor.attributes[VertexAttribute.position.rawValue].bufferIndex = BufferIndex.meshPositions.rawValue
 
-		vertexDescriptor.attributes[VertexAttribute.texcoord.rawValue].format = MTLVertexFormat.float2
+		vertexDescriptor.attributes[VertexAttribute.texcoord.rawValue].format = .float2
 		vertexDescriptor.attributes[VertexAttribute.texcoord.rawValue].offset = 0
 		vertexDescriptor.attributes[VertexAttribute.texcoord.rawValue].bufferIndex = BufferIndex.meshTexcoords.rawValue
 
 		vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stride = 12
 		vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepRate = 1
-		vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepFunction = MTLVertexStepFunction.perVertex
+		vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepFunction = .perVertex
 
 		vertexDescriptor.layouts[BufferIndex.meshTexcoords.rawValue].stride = 8
 		vertexDescriptor.layouts[BufferIndex.meshTexcoords.rawValue].stepRate = 1
-		vertexDescriptor.layouts[BufferIndex.meshTexcoords.rawValue].stepFunction = MTLVertexStepFunction.perVertex
+		vertexDescriptor.layouts[BufferIndex.meshTexcoords.rawValue].stepFunction = .perVertex
 
 		return vertexDescriptor
 	}
