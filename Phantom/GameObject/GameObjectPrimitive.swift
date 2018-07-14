de44ff5c1e6b824ba07b0ba43e9d070b77001e6c
HaijianLiu <haijanliu@gmail.com>
2018-07-15 01:21:26 +0900

    Create gameobjects by shader types.

--- Phantom/GameObject/GameObjectPrimitive.swift
+++ Phantom/GameObject/GameObjectPrimitive.swift
@@ -11,21 +11,23 @@ extension GameObject {
 	/// Creates a primitive gameobject in the shape of a rectangular box or cube.
 	///
 	/// - Parameters:
+	///   - shaderType: A shader type defined by the default shaders library.
 	///   - dimensions: A vector containing the width (x-component), height (y-component), and depth (z-component) of the box to generate. If all components are equal, this method generates a cube.
 	///   - segments: The number of points to generate along each dimension. A larger number of points increases rendering fidelity but decreases rendering performance.
 	///   - geometryType: The type of geometric primitive from which to construct the mesh; must be either kindTriangles, kindQuads, or lines.
 	///   - inwardNormals: true to generate normal vectors pointing toward the inside of the box; false to generate normal vectors pointing outward.
 	/// - Returns: A new GameObject with MeshRenderer component.
-	public static func createBox(withDimensions dimensions: Vector3 = Vector3(1), segments: Uint3 = Uint3(1), geometryType: GeometryType = .triangles, inwardNormals: Bool = false) -> GameObject? {
+	public static func createBox(shaderType: ShaderType = ShaderType.primitive, withDimensions dimensions: Vector3 = Vector3(1), segments: Uint3 = Uint3(1), geometryType: GeometryType = .triangles, inwardNormals: Bool = false) -> GameObject? {
 		guard let device = View.main.device else { return nil }
 		let mtkMeshBufferAllocator = MTKMeshBufferAllocator(device: device)
 		let mdlMesh = MDLMesh.newBox(withDimensions: dimensions, segments: segments, geometryType: geometryType, inwardNormals: inwardNormals, allocator: mtkMeshBufferAllocator)
-		return GameObject.createMeshGameObject(device, with: mdlMesh)
+		return GameObject.createMeshGameObject(device, with: mdlMesh, shaderType: shaderType)
 	}
 	
 	/// Creates a primitive gameobject in the shape of an ellipsoid or sphere.
 	///
 	/// - Parameters:
+	///   - shaderType: A shader type defined by the default shaders library.
 	///   - radius: A vector containing the width (x-component), height (y-component), and depth (z-component) of the bounding box of the ellipsoid to generate. If all components are equal, this method generates a sphere.
 	///   - radialSegments: The number of points to generate around the horizontal circumference of the ellipsoid (that is, its cross-section in the xz-plane). A larger number of points increases rendering fidelity but decreases rendering performance.
 	///   - verticalSegments: The number of points to generate along the height of the ellipsoid. A larger number of points increases rendering fidelity but decreases rendering performance.
@@ -33,25 +35,26 @@ extension GameObject {
 	///   - inwardNormals: true to generate normal vectors pointing toward the center of the ellipsoid; false to generate normal vectors pointing outward.
 	///   - hemisphere: true to generate only the upper half of the ellipsoid or sphere (a dome); false to generate a complete ellipsoid or sphere.
 	/// - Returns: A new GameObject with MeshRenderer component.
-	public static func createEllipsoid(withRadii radius: Vector3 = Vector3(1), radialSegments: Int = 24, verticalSegments: Int = 18, geometryType: GeometryType = .triangles, inwardNormals: Bool = false, hemisphere: Bool = false) -> GameObject? {
+	public static func createEllipsoid(shaderType: ShaderType = ShaderType.primitive, withRadii radius: Vector3 = Vector3(1), radialSegments: Int = 24, verticalSegments: Int = 18, geometryType: GeometryType = .triangles, inwardNormals: Bool = false, hemisphere: Bool = false) -> GameObject? {
 		guard let device = View.main.device else { return nil }
 		let mtkMeshBufferAllocator = MTKMeshBufferAllocator(device: device)
 		let mdlMesh = MDLMesh.newEllipsoid(withRadii: radius, radialSegments: radialSegments, verticalSegments: verticalSegments, geometryType: geometryType, inwardNormals: inwardNormals, hemisphere: hemisphere, allocator: mtkMeshBufferAllocator)
-		return GameObject.createMeshGameObject(device, with: mdlMesh)
+		return GameObject.createMeshGameObject(device, with: mdlMesh, shaderType: shaderType)
 	}
 
 	/// Creates a primitive gameobject in the shape of a rectangular plane.
 	///
 	/// - Parameters:
+	///   - shaderType: A shader type defined by the default shaders library.
 	///   - dimensions: A vector containing the width (x-component) and depth (y-component) of the plane to generate.
 	///   - segments: The number of points to generate along each dimension. A larger number of points increases rendering fidelity but decreases rendering performance.
 	///   - geometryType: The type of geometric primitive from which to construct the mesh; must be either kindTriangles or
 	/// - Returns: A new GameObject with MeshRenderer component.
-	public static func createPlane(withDimensions dimensions: Vector2 = Vector2(10), segments: Uint2 = Uint2(10), geometryType: GeometryType = .triangles) -> GameObject? {
+	public static func createPlane(shaderType: ShaderType = ShaderType.primitive, withDimensions dimensions: Vector2 = Vector2(10), segments: Uint2 = Uint2(10), geometryType: GeometryType = .triangles) -> GameObject? {
 		guard let device = View.main.device else { return nil }
 		let mtkMeshBufferAllocator = MTKMeshBufferAllocator(device: device)
 		let mdlMesh = MDLMesh.newPlane(withDimensions: dimensions, segments: segments, geometryType: geometryType, allocator: mtkMeshBufferAllocator)
-		return GameObject.createMeshGameObject(device, with: mdlMesh)
+		return GameObject.createMeshGameObject(device, with: mdlMesh, shaderType: shaderType)
 	}
 
 	/// Create a basic gameobject with MDLMesh.
@@ -60,12 +63,12 @@ extension GameObject {
 	///   - device: MTLDevice
 	///   - mdlMesh: MDLMesh
 	/// - Returns: GameObject?
-	private static func createMeshGameObject(_ device: MTLDevice, with mdlMesh: MDLMesh) -> GameObject? {
+	private static func createMeshGameObject(_ device: MTLDevice, with mdlMesh: MDLMesh, shaderType: ShaderType) -> GameObject? {
 		// Create GameObject with MeshRenderer component.
 		guard let gameObject = GameObject() else { return nil }
 		guard let meshRenderer: MeshRenderer = gameObject.addComponent() else { return nil }
 		// Attach material
-		guard let shader = Shader(device) else { return nil }
+		guard let shader = Shader(device, shaderType) else { return nil }
 		let material = Material(with: shader)
 		meshRenderer.material = material
 		// TODO: refactor.
