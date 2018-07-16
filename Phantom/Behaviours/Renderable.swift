f20ea9dab1b267166dd2fbf63b8c68a52231b240
HaijianLiu <haijanliu@gmail.com>
2018-07-16 23:00:39 +0900

    No importing.

--- Phantom/Behaviours/Renderable.swift
+++ Phantom/Behaviours/Renderable.swift
@@ -1,7 +1,5 @@
 // Copyright © haijian. All rights reserved.
 
-import MetalKit
-
 /// Renderable Behaviour protocol to encode draw commands.
 /// These behaviours will be stored in render pass instance as weak references,
 /// and update encoding during render pass executing draw function.
