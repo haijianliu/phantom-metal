cb916417f78b21e3bcae8194957201289669d483
HaijianLiu <haijanliu@gmail.com>
2018-07-21 18:31:00 +0900

    Add gameobject parent extenstion and add child function.

--- Phantom/GameObject/GameObjectParent.swift
+++ Phantom/GameObject/GameObjectParent.swift
@@ -1,3 +1,16 @@
 // Copyright © haijian. All rights reserved.
 
-import Foundation
+// MARK: - Parent extension.
+extension GameObject {
+	/// Add a gameobject(as child) to self(as parent).
+	///
+	/// One gameobject can only have one parent at one time. If you reassign a gameobjec to another, this action will break the link between the old one automatically.
+	/// - Parameter gameObject: GameObject as child.
+	public func addChild(_ gameObject: GameObject) {
+		// TODO: reassign and register to renderer directly.
+		children.append(gameObject)
+		gameObject.parent = self
+	}
+	
+	// TODO: delete parent and child.
+}
