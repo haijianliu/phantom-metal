2ed960359b788fd5ac166e2a3461b1ee835985fb
HaijianLiu <haijanliu@gmail.com>
2018-07-21 18:38:49 +0900

    Update samples: a sample of hierachy.

--- Samples/SampleApplication.swift
+++ Samples/SampleApplication.swift
@@ -4,59 +4,44 @@ import PhantomKit
 
 class SampleApplication: ApplicationDelegate {
 	func start() {
-
 		// Create cube.
 		cube: do {
-			// GameObject
 			guard let gameObject = GameObject.createBox(withDimensions: Vector3(2, 2, 2)) else { break cube }
-			// Transform
 			gameObject.transform.position = Vector3(x: 2.5, y: 2.5, z: 0)
-			// Set material.
-			gameObject.material?.texture = Texture(name: "UV_Grid_Lrg")
-			// SampleBehaviour
-			guard let _: SampleBehaviour = gameObject.addComponent() else { break cube }
-			// Add GameObject
-			Application.addGameObject(gameObject)
+			gameObject.material?.texture = Texture(name: "UV_Grid_Lrg") // Set material.
+			Application.addGameObject(gameObject) // Register to renderer.
 		}
 
 		// Create plane.
 		plane: do {
-			// GameObject
 			guard let gameObject = GameObject.createPlane(withDimensions: Vector2(20, 20), segments: Uint2(20, 20)) else { break plane }
-			// Set material.
-			gameObject.material?.texture = Texture(name: "UV_Grid_Sm")
-			gameObject.material?.fillMode = .lines
-			// SampleBehaviour
-			guard let _: SampleBehaviour = gameObject.addComponent() else { break plane }
-			// Add GameObject
-			Application.addGameObject(gameObject)
+			gameObject.material?.texture = Texture(name: "UV_Grid_Sm") // Set texture.
+			gameObject.material?.fillMode = .lines // Set fill mode.
+			Application.addGameObject(gameObject) // Register to renderer.
 		}
 		
 		// Create ellipsoid.
 		ellipsoid: do {
-			// GameObject
 			guard let gameObject = GameObject.createEllipsoid(shaderType: ShaderType.normalColor) else { break ellipsoid }
-			// Transform
 			gameObject.transform.position = Vector3(x: -2.5, y: 2.5, z: 0)
 			gameObject.transform.scale = Vector3(1.4)
-			// Set material.
-			gameObject.material?.texture = Texture(name: "UV_Grid_Sm")
-			// SampleBehaviour
-			guard let _: SampleBehaviour = gameObject.addComponent() else { break ellipsoid }
-			// Add GameObject
-			Application.addGameObject(gameObject)
+			gameObject.material?.texture = Texture(name: "UV_Grid_Sm") // Set material.
+			Application.addGameObject(gameObject) // Register to renderer.
 		}
 		
 		// Create camera.
 		camera: do {
-			// GameObject
+			// Root gameobject.
 			guard let gameObject = GameObject() else { break camera }
-			// Camera
-			guard let _: Camera = gameObject.addComponent() else { break camera }
-			// Transform.
-			gameObject.transform.position = Vector3(0, 10, 10);
+			gameObject.transform.position = Vector3(0, 0, 0); // Transform.
+			guard let _: SampleBehaviour = gameObject.addComponent() else { break camera }
+			// Camera child gameobject attached to root gameobject.
+			guard let cameraObject = GameObject() else { break camera } // TODO: GameObject.createCamera(main???).
+			guard let _: Camera = cameraObject.addComponent() else { break camera }
+			cameraObject.transform.position = Vector3(0, 10, 10); // Transform.
+			gameObject.addChild(cameraObject) // Add camera node to root gameobject.
 			// Add GameObject
-			Application.addGameObject(gameObject)
+			Application.addGameObject(gameObject) // Register to renderer.
 		}
 	}
 }
