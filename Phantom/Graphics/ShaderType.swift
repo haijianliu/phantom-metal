e7cc5b31c7d2a6bb39b374f7f60ef1e6a9f8915e
HaijianLiu <haijanliu@gmail.com>
2018-07-22 22:35:16 +0900

    Add computed function constant values to shader type. (TODO: deallocate unsafe pointers)

--- Phantom/Graphics/ShaderType.swift
+++ Phantom/Graphics/ShaderType.swift
@@ -1,5 +1,7 @@
 // Copyright © haijian. All rights reserved.
 
+import MetalKit
+
 public enum ShaderType {
 	
 	case standard
@@ -7,10 +9,8 @@ public enum ShaderType {
 	
 	internal var vertex: String {
 		switch self {
-		case .standard:
+		case .standard, .normalColor:
 			return "standardVertex"
-		case .normalColor:
-			return "standardNoTextureVertex"
 		}
 	}
 	
@@ -21,5 +21,28 @@ public enum ShaderType {
 		case .normalColor:
 			return "normalColorFragment"
 		}
+	}
+	
+	private var functionConstants: ContiguousArray<Bool> {
+		var functionContants = ContiguousArray<Bool>(repeating: false, count: FunctionConstant.count.rawValue)
+		switch self {
+		case .standard:
+			functionContants[FunctionConstant.baseColorMapIndex.rawValue] = true
+		default: break
+		}
+		return functionContants
+	}
+	
+	internal var functionConstantValues: MTLFunctionConstantValues {
+		var functionConstants = self.functionConstants
+		let functionConstantValues = MTLFunctionConstantValues()
+		for index in 0 ..< FunctionConstant.count.rawValue {
+			let mutablePointer = UnsafeMutablePointer<Bool>.allocate(capacity: 1)
+			mutablePointer.initialize(to: functionConstants[index])
+			let rawPointer = UnsafeRawPointer(mutablePointer)
+			functionConstantValues.setConstantValue(rawPointer, type: .bool, index: index)
+		}
+		// TODO: deallocate.
+		return functionConstantValues
 	}
 }
