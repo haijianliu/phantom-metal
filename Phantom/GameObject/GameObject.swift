cb6fbb4680326f9a603c36f5b51a0774db6ae7be
HaijianLiu <haijanliu@gmail.com>
2018-07-24 22:48:44 +0900

    Create gameobject component extension.

--- Phantom/GameObject/GameObject.swift
+++ Phantom/GameObject/GameObject.swift
@@ -46,31 +46,9 @@ public class GameObject {
 		// Transform
 		guard let _: Transform = self.addComponent() else { return nil }
 	}
-	
-	/// Adds a component class named type name to the game object.
-	///
-	/// If there is already a same type of componet added, this function will do nothing, and return a nil
-	/// - Returns: Componet instance if succeed, otherwise nil
-	public func addComponent<ComponentType: Component>() -> ComponentType? {
-		let typeName = String(describing: ComponentType.self)
-		if components[typeName] == nil {
-			let component = ComponentType(self)
-			components[typeName] = component
-			return components[typeName] as? ComponentType // TODO: no optional type return.
-		} else {
-			return nil
-		}
-	}
-	
-	/// Get a component instance attached to the game object by component type. Calling this function during real time updating is not recommended.
-	/// - Returns: Component instance of component type if the game object has one attached, nil if it doesn't.
-	public func getComponent<ComponentType: Component>() -> ComponentType? {
-		return components[String(describing: ComponentType.self)] as? ComponentType
-	}
 }
 
 extension GameObject: RenderEncodable {
-	
 	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
 		// TODO: in transform
 		// TODO: in game object
