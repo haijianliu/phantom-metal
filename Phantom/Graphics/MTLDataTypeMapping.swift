49501a02e956fb69cb94b2f881b5105e2bb4cc3d
HaijianLiu <haijanliu@gmail.com>
2018-07-26 02:36:34 +0900

    Add MTLDataType extension which is mapping information from MTLDataType to MTLVertexFormat.

--- Phantom/Graphics/MTLDataTypeMapping.swift
+++ Phantom/Graphics/MTLDataTypeMapping.swift
@@ -1,3 +1,31 @@
 // Copyright © haijian. All rights reserved.
 
-import Foundation
+import MetalKit
+
+// MARK: - MTLDataType type mapping extension.
+extension MTLDataType {
+	
+	/// Values that specify the organization of function vertex data.
+	var format: MTLVertexFormat {
+		switch self {
+		case .float2:
+			return MTLVertexFormat.float2
+		case .float3:
+			return MTLVertexFormat.float3
+		default:
+			return MTLVertexFormat.invalid
+		}
+	}
+	
+	/// The distance, in bytes, between the attribute data of two vertices in the buffer.
+	var stride: Int {
+		switch self {
+		case .float2:
+			return MemoryLayout<Float>.stride * 2
+		case .float3:
+			return MemoryLayout<Float>.stride * 3
+		default:
+			return 0
+		}
+	}
+}
