c31d3f47190fd9fdf360277b8542367d36ef8a3a
HaijianLiu <haijanliu@gmail.com>
2018-07-25 23:39:04 +0900

    Add pre-stored vertex buffer indices array for render encoding.

--- Phantom/Graphics/Mesh.swift
+++ Phantom/Graphics/Mesh.swift
@@ -3,9 +3,22 @@
 import MetalKit
 
 class Mesh {
-	var mtkMesh: MTKMesh
+	/// A container for the vertex data of a Model I/O mesh, suitable for use in a Metal app.
+	private var mtkMesh: MTKMesh
+	
+	/// Stroed vertex attribuite indices array for render encoding.
+	private var vertexBufferIndices = ContiguousArray<Int>()
+	
+	/// The vertex winding rule that determines a front-facing primitive (Default: counter-clockwise).
 	var winding: MTLWinding = MTLWinding.counterClockwise
-	init(with mtkMesh: MTKMesh) { self.mtkMesh = mtkMesh }
+
+	init(with mtkMesh: MTKMesh) {
+		self.mtkMesh = mtkMesh
+		for (index, element) in mtkMesh.vertexDescriptor.layouts.enumerated() {
+			guard let layout = element as? MDLVertexBufferLayout else { return }
+			if layout.stride != 0 { vertexBufferIndices.append(index) }
+		}
+	}
 }
 
 extension Mesh: RenderEncodable {
@@ -13,14 +26,9 @@ extension Mesh: RenderEncodable {
 		// Encode winding order of front-facing primitives.
 		renderCommandEncoder.setFrontFacing(winding)
 		// Encode a buffer for the vertex shader function.
-		for (index, element) in mtkMesh.vertexDescriptor.layouts.enumerated() {
-			guard let layout = element as? MDLVertexBufferLayout else { return }
-			if layout.stride != 0 {
-				let vertexBuffers = mtkMesh.vertexBuffers[index]
-				renderCommandEncoder.setVertexBuffer(vertexBuffers.buffer, offset: vertexBuffers.offset, index: index)
-			} else {
-				break
-			}
+		for index in vertexBufferIndices {
+			let vertexBuffers = mtkMesh.vertexBuffers[index]
+			renderCommandEncoder.setVertexBuffer(vertexBuffers.buffer, offset: vertexBuffers.offset, index: index)
 		}
 		// Encode draw command.
 		for submesh in mtkMesh.submeshes {
