8ecc0d54cb8982242a2ea726826ad162f4cbe07f
HaijianLiu <haijanliu@gmail.com>
2018-07-29 15:26:40 +0900

    Add documentary.

--- Phantom/Framework/ApplicationDelegate.swift
+++ Phantom/Framework/ApplicationDelegate.swift
@@ -1,5 +1,7 @@
 // Copyright © haijian. All rights reserved.
 
+/// A set of methods that delegates of Application objects can implement.
 public protocol ApplicationDelegate: AnyObject {
+	/// Invoked after the application has been launched and initialized but before it has received its first event.
 	func start()
 }
