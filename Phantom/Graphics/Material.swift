3ae7137a1495d3b20ff643f438d010e7639757b3
HaijianLiu <haijanliu@gmail.com>
2018-07-30 00:14:08 +0900

    Refactor initializer.

--- Phantom/Graphics/Material.swift
+++ Phantom/Graphics/Material.swift
@@ -4,20 +4,20 @@ import MetalKit
 
 // TODO: struct only protocol
 // TODO: when add drawable behaviour to list, check if there is shader and texture attached. If not, assign one.
+/// All properties from a material.
 public class Material {
-	var shader: Shader
 	public var texture: Texture?
 	public var cullMode: MTLCullMode = MTLCullMode.back
 	public var fillMode: MTLTriangleFillMode = MTLTriangleFillMode.fill
-
-	init(with shader: Shader) { self.shader = shader }
+	/// The shader used by the material.
+	let shader = Shader()
 }
 
 extension Material: RenderEncodable {
 	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
 		renderCommandEncoder.setCullMode(cullMode)
 		renderCommandEncoder.setTriangleFillMode(fillMode)
-		shader.encode(to: renderCommandEncoder)
 		texture?.encode(to: renderCommandEncoder)
+		shader.encode(to: renderCommandEncoder)
 	}
 }
