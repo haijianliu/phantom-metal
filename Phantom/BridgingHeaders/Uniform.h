f30ebd6a77b615bd64f195f9e86df3a5d81b0078
HaijianLiu <haijanliu@gmail.com>
2018-08-08 23:45:09 +0900

    Add light structrues and light function contants that shared by MetalKit and Metal.

--- Phantom/BridgingHeaders/Uniform.h
+++ Phantom/BridgingHeaders/Uniform.h
@@ -15,6 +15,7 @@
 // https://developer.apple.com/documentation/scenekit/scnprogram
 // https://forum.unity.com/threads/world-space-normal.58810/
 
+// DOTO: refactor names.
 typedef NS_ENUM(NSInteger, FunctionConstant)
 {
 	FunctionConstantBaseColorMapIndex,
@@ -23,7 +24,8 @@ typedef NS_ENUM(NSInteger, FunctionConstant)
 	FunctionConstantRoughnessMapIndex,
 	FunctionConstantAmbientOcclusionMapIndex,
 	FunctionConstantIrradianceMapIndex,
-	FunctionConstantCount
+	FunctionConstantLightIndex,
+	FunctionConstantCount,
 };
 
 /// An enum that describes how vertex data is organized and mapped to a vertex function. And used by MTLVertexDescriptor and MDLVertexDescriptor to configure how vertex data stored in memory is mapped to attributes in a vertex shader.
@@ -32,20 +34,25 @@ typedef NS_ENUM(NSInteger, VertexAttribute)
 	VertexAttributePosition = 0,
 	VertexAttributeTexcoord,
 	VertexAttributeNormal,
-	VertexAttributeCount
+	VertexAttributeCount,
 };
 
+/// Bridge structrue between MetalKit and Metal. Indices are used as arguments in Matel and indices when set vertex or fragment buffer is called in MetalKit.
 /// An enum that describes how buffer data is organized and mapped to shader functions. Raw values continue to vertex attribute indices which is invalid for buffer objects.
 typedef NS_ENUM(NSInteger, BufferIndex)
 {
 	BufferIndexNodeBuffer = VertexAttributeCount,
+	BufferIndexLightBuffer,
+	BufferIndexCount,
 };
 
 // TODO: Add format and stride information.
 
+// TODO: add more texture types.
 typedef NS_ENUM(NSInteger, TextureIndex)
 {
 	TextureIndexColor,
+	TextureIndexCount,
 };
 
 // TODO: Add Scene buffer.
@@ -58,6 +65,20 @@ typedef struct
 	matrix_float4x4 modelMatrix;
 	matrix_float4x4 inverseTransposeModelMatrix;
 } StandardNodeBuffer;
+
+/// Light uniforms.
+typedef struct
+{
+	vector_float3 position;
+	vector_float3 color;
+	float intensity;
+} LightData;
+
+typedef struct
+{
+	LightData light[16];
+	int count;
+} LightBuffer;
 
 // TODO: Add no normal node buffer. Maybe instance particle type?
 
