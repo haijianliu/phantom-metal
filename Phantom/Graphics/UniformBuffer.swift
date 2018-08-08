902fa7c7f81d680c0f0a1605c623c6c02c822f09
HaijianLiu <haijanliu@gmail.com>
2018-08-09 00:15:03 +0900

    Add an extension of light buffer for setting up light data.

--- Phantom/Graphics/UniformBuffer.swift
+++ Phantom/Graphics/UniformBuffer.swift
@@ -2,17 +2,28 @@
 
 protocol UniformBuffer { }
 
-protocol NodeBuffer: UniformBuffer {
-	mutating func update(by transform: Transform)
-}
+//protocol NodeBuffer: UniformBuffer {
+//	mutating func update(by transform: Transform)
+//}
+//
+//protocol SceneBuffer: UniformBuffer {
+//	mutating func update()
+//}
 
-protocol SceneBuffer: UniformBuffer {
-	mutating func update(by camera: Camera)
-}
+// TODO: confirm protocol.
 
-extension StandardNodeBuffer: NodeBuffer {
+extension StandardNodeBuffer: UniformBuffer {
 	mutating func update(by transform: Transform) {
 		self.modelMatrix = transform.localToWorldMatrix
 		self.inverseTransposeModelMatrix = transform.localToWorldMatrix.inverse.transpose
+	}
+}
+
+extension LightBuffer: UniformBuffer {
+	// TODO: use contigous array.
+	mutating func update(_ lightDatas: [LightData]) {
+		self.count = Int32(lightDatas.count)
+		let pointer = UnsafeMutableBufferPointer<LightData>(start: &self.light.0, count: 16)
+		for (index, lightData) in lightDatas.enumerated() { pointer[index] = lightData }
 	}
 }
