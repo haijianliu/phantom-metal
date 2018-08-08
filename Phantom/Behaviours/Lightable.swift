098e30d8ff25dd47f0231641cbf00f5da5d52893
HaijianLiu <haijanliu@gmail.com>
2018-08-08 23:56:49 +0900

    Add Lightable behaviour protocol.

--- Phantom/Behaviours/Lightable.swift
+++ Phantom/Behaviours/Lightable.swift
@@ -1,3 +1,9 @@
 // Copyright © haijian. All rights reserved.
 
-import Foundation
+/// Lightable behaviours encodes light buffer separately from renderables.
+/// These behaviours will be stored in background render pass (TODO) (QoS: background) instance as weak references,
+/// and update encoding light buffers per scene, during render pass executing draw function.
+@objc protocol Lightable: Behaviour {
+	/// Shared light data structrue shared by MetalKit and Metal (Read only).
+	var lightData: LightData { get }
+}
