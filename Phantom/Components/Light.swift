0b1b193a356e4b6eee1dc9aa3b40837a7620c74e
HaijianLiu <haijanliu@gmail.com>
2018-08-09 00:03:06 +0900

    Add light component.

--- Phantom/Components/Light.swift
+++ Phantom/Components/Light.swift
@@ -1,3 +1,18 @@
 // Copyright © haijian. All rights reserved.
 
-import Foundation
+public class Light: Component, Lightable {
+	/// The color of the light.
+	public var color = Vector3(1)
+	/// The Intensity of a light is multiplied with the Light color.
+	public var intensity: Float = 1
+	
+	// TODO: use dirty.
+	/// Shared light data structrue shared by MetalKit and Metal (Read only).
+	var lightData: LightData {
+		var lightData = LightData()
+		lightData.color = color
+		lightData.intensity = intensity
+		lightData.position = transform.position
+		return lightData
+	}
+}
