9b6eee06c0b3fdd295a967520340badf6966d2e5
HaijianLiu <haijanliu@gmail.com>
2018-08-12 03:01:54 +0900

    Drawable confirms objc.

--- Phantom/Graphics/Drawable.swift
+++ Phantom/Graphics/Drawable.swift
@@ -2,8 +2,8 @@
 
 import MetalKit
 
-/// The drawable protocol to be used for committing command buffer to draw in the current frame. Comformed by render pass and executed in main view.
-protocol Drawable {
+/// The drawable protocol to be used for present command buffer to draw in the current frame. Comformed by render pass and executed in main view.
+@objc protocol Drawable {
 	/// Commits this command buffer for execution as soon as possible.
 	///
 	/// Every time call this draw method, the command buffer will be committed in the main view synchronously and executed by the GPU. So only use this command buffer to create command encoder.
