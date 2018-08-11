557630a36d7f1355d5d8411d8234dc77a967f99e
HaijianLiu <haijanliu@gmail.com>
2018-08-12 02:59:02 +0900

    Add renderpass super class.

--- Phantom/Graphics/RenderPass.swift
+++ Phantom/Graphics/RenderPass.swift
@@ -1,3 +1,22 @@
 // Copyright © haijian. All rights reserved.
 
-import Foundation
+import MetalKit
+
+class RenderPass: Drawable {
+	var renderableBehaviours = ContiguousArray<Weak<Renderable>>()
+	
+	var depthStencilState: MTLDepthStencilState
+	
+	required convenience init?(device: MTLDevice) {
+		let depthStencilDescriptor = MTLDepthStencilDescriptor()
+		self.init(device: device, depthStencilDescriptor: depthStencilDescriptor)
+	}
+	
+	init?(device: MTLDevice, depthStencilDescriptor: MTLDepthStencilDescriptor) {
+		guard let newDepthStencilState = device.makeDepthStencilState(descriptor: depthStencilDescriptor) else { return nil }
+		depthStencilState = newDepthStencilState
+		renderableBehaviours.reserveCapacity(0xFF)
+	}
+
+	func draw(in view: MTKView, by commandBuffer: MTLCommandBuffer) { }
+}
