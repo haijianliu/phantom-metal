5c9e24af29f9e6712017046c78047ece7781ba30
HaijianLiu <haijanliu@gmail.com>
2018-08-12 18:24:21 +0900

    Add shadow renderer.

--- Phantom/Components/ShadowRenderer.swift
+++ Phantom/Components/ShadowRenderer.swift
@@ -1,3 +1,18 @@
 // Copyright © haijian. All rights reserved.
 
-import Foundation
+import MetalKit
+
+/// Renders meshes.
+class ShadowRenderer: Renderer, Renderable {
+	
+	let mesh = Mesh()
+	
+	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
+		// Material encoding: including texture and shader encoding.
+		material.encode(to: renderCommandEncoder)
+		// Game object encoding: update triple buffer.
+		gameObject.encode(to: renderCommandEncoder)
+		// Mesh encoding: contents draw call encoding, which must be encoded at last (just before end encoding).
+		mesh.encode(to: renderCommandEncoder)
+	}
+}
