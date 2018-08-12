b1bcf0d6f124690cb5ad62e27eb89f38a0350997
HaijianLiu <haijanliu@gmail.com>
2018-08-12 18:15:28 +0900

    Add function constant for normal inout.

--- Phantom/BridgingHeaders/Uniform.h
+++ Phantom/BridgingHeaders/Uniform.h
@@ -25,6 +25,7 @@ typedef NS_ENUM(NSInteger, FunctionConstant)
 	FunctionConstantAmbientOcclusionMapIndex,
 	FunctionConstantIrradianceMapIndex,
 	FunctionConstantLightIndex,
+	FunctionConstantNormalIndex,
 	FunctionConstantCount,
 };
 
