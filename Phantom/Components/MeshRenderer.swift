6ee1d4c67fbf92c2da9b00a24d4b1466ca155bf7
HaijianLiu <haijanliu@gmail.com>
2018-08-26 14:26:09 +0900

    Confirm registrable.

--- Phantom/Components/MeshRenderer.swift
+++ Phantom/Components/MeshRenderer.swift
@@ -4,12 +4,12 @@ import MetalKit
 
 // TODO: main renderer for initialize other renderers.
 /// Renders meshes.
-class MeshRenderer: Renderer, Renderable {
-	
+class MeshRenderer: Renderer, Renderable, Registrable {
 	// TODO: multiple.
 	/// Mesh slot for rendering.
 	let mesh = Mesh()
 	
+	// TODO: didset.
 	// TODO: [shadowCastingMode](https://docs.unity3d.com/ScriptReference/Renderer-shadowCastingMode.html)
 	/// Does this object cast shadows?
 	var castShadows: Bool = true
@@ -17,6 +17,18 @@ class MeshRenderer: Renderer, Renderable {
 	/// Does this object receive shadows?
 	/// Note that receive shadows flag is not used when using one of Deferred rendering paths; all objects receive shadows there. (TODO)
 	var receiveShadows: Bool = true
+	
+	func register() {
+		material.shader.load()
+		mesh.load(from: material.shader.vertexDescriptor)
+		// If mesh casts shadows, add a new shadow renderer component and add this render behaviour will be added to shadowmap renderpass.
+		if castShadows == true {
+			let _: ShadowRenderer? = self.gameObject.addComponent()
+			guard let shadowRenderer: ShadowRenderer = gameObject.getComponent() else { return }
+			shadowRenderer.mesh.mdlMesh = mesh.mdlMesh // TODO: refactor
+			shadowRenderer.register()
+		}
+	}
 	
 	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
 		// Material encoding: including texture and shader encoding.
--- Phantom/Components/ShadowRenderer.swift
+++ Phantom/Components/ShadowRenderer.swift
@@ -3,9 +3,15 @@
 import MetalKit
 
 /// Renders meshes.
-class ShadowRenderer: Renderer, Renderable {
+class ShadowRenderer: Renderer, Renderable, Registrable {
 	
 	let mesh = Mesh()
+	
+	func register() {
+		material.shader.shaderType = .shadowMap
+		material.shader.load()
+		mesh.load(from: material.shader.vertexDescriptor)
+	}
 	
 	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
 		// Material encoding: including texture and shader encoding.
