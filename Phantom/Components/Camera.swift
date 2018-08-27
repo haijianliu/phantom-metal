a9e774a10df76176bd5d407d65fc6290ba0ae084
HaijianLiu <haijanliu@gmail.com>
2018-08-27 23:37:12 +0900

    Camera component confirms updatable and render encodable.

--- Phantom/Components/Camera.swift
+++ Phantom/Components/Camera.swift
@@ -1,11 +1,16 @@
 // Copyright © haijian. All rights reserved.
 
-public class Camera: Component {
-	
+import MetalKit
+
+public class Camera: Component, Updatable, RenderEncodable {
 	/// The last enabled camera tagged GameObject.mainCamera (Read Only).
 	///
 	/// Returns **nil** if there is no such camera in the scene.
 	public internal(set) weak static var main: Camera?
+	
+	weak static var shadow: Camera?
+	
+	private var cameraUniformBuffer: TripleBuffer<CameraBuffer>
 	
 	/// The near clipping plane distance.
 	var nearClipPlane: Float = 0.1
@@ -34,12 +39,26 @@ public class Camera: Component {
 	///If you change this matrix, the camera no longer updates its rendering based on its fieldOfView. This lasts until you call ResetProjectionMatrix.
 	var projectionMatrix: Matrix4x4 = Matrix4x4()
 	
-	required public init(_ gameObject: GameObject) {
-		super.init(gameObject)! // TODO: safe wrap.
+	required public init?(_ gameObject: GameObject) {
+		guard let device = Application.sharedInstance.device else { return nil }
+		// TODO: init dynamic semaphore value
+		guard let newBuffer = TripleBuffer<CameraBuffer>(device) else { return nil }
+		cameraUniformBuffer = newBuffer
+		
+		super.init(gameObject) // TODO: safe wrap.
 		if Camera.main == nil {
 			gameObject.tag = .mainCamera
 			Camera.main = self
 		}
+	}
+	
+	public func update() {
+		cameraUniformBuffer.data.update(by: self)
+		cameraUniformBuffer.endWritting()
+	}
+	
+	func encode(to renderCommandEncoder: MTLRenderCommandEncoder) {
+		renderCommandEncoder.setVertexBuffer(cameraUniformBuffer.buffer, offset: cameraUniformBuffer.offset, index: BufferIndex.cameraBuffer.rawValue)
 	}
 }
 
