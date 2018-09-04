bdf671987bbd076494881505a2b94902f449665b
HaijianLiu <haijanliu@gmail.com>
2018-09-04 23:27:02 +0900

    Add function: addRenderPass.

--- Phantom/Application/Application.swift
+++ Phantom/Application/Application.swift
@@ -21,4 +21,16 @@ public class Application {
 	// TODO: remove
 	/// MTKViewDelegat reference holder.
 	var viewDelegate = ViewDelegate()
+	
+	static func addRenderPass<RenderPassType: RenderPass>() -> RenderPassType? {
+		let typeName = String(describing: RenderPassType.self)
+		guard let scene = Application.sharedInstance.scene else { return nil }
+		if scene.renderPasses[typeName] == nil {
+			guard let device = Application.sharedInstance.device else { return nil }
+			guard let renderPass = RenderPassType(device: device) else { return nil }
+			scene.renderPasses[typeName] = renderPass
+			Application.sharedInstance.viewDelegate.addRenderPass(renderPass)
+		}
+		return scene.renderPasses[typeName] as? RenderPassType
+	}
 }
