7d475169cfbd1a87ae45b6a5d2647dd5a4ae4eb3
HaijianLiu <haijanliu@gmail.com>
2018-09-09 14:24:29 +0900

    Main renderpass no longer uses the current drawable's renderpass descriptor.

--- Phantom/Graphics/MainRenderPass.swift
+++ Phantom/Graphics/MainRenderPass.swift
@@ -13,10 +13,42 @@ class MainRenderPass: RenderPass {
 		super.init(device: device, depthStencilDescriptor: depthStencilDescriptor)
 	}
 	
+	override func register() {
+		let textureDescriptor = MTLTextureDescriptor()
+		textureDescriptor.height = 1200 // TODO: set size.
+		textureDescriptor.width = 1600
+//		textureDescriptor.sampleCount = ShaderType.standard.sampleCount
+		textureDescriptor.usage = [.shaderRead, .renderTarget]
+		textureDescriptor.pixelFormat = ShaderType.standard.colorAttachmentsPixelFormat[0]
+		textureDescriptor.resourceOptions = .storageModePrivate
+		textureDescriptor.mipmapLevelCount = 5
+		guard let color = depthStencilState.device.makeTexture(descriptor: textureDescriptor) else { return }
+		targets.append(color)
+		textureDescriptor.pixelFormat = ShaderType.standard.depthAttachmentPixelFormat
+		guard let depth = depthStencilState.device.makeTexture(descriptor: textureDescriptor) else { return }
+		targets.append(depth)
+		
+		// TODO: customize this function varying from render passes.
+		renderPassDescriptor.colorAttachments[0].storeAction = .store
+		renderPassDescriptor.colorAttachments[0].loadAction = .clear
+		renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0, 0, 0)
+		renderPassDescriptor.colorAttachments[0].texture = targets[0] // TODO: for loop.
+		renderPassDescriptor.depthAttachment.storeAction = .store
+		renderPassDescriptor.depthAttachment.loadAction = .clear
+		renderPassDescriptor.depthAttachment.clearDepth = 1
+		renderPassDescriptor.depthAttachment.texture = targets[1] // TODO: for loop.
+//		renderPassDescriptor.stencilAttachment.storeAction = .dontCare
+//		renderPassDescriptor.stencilAttachment.loadAction = .clear
+//		renderPassDescriptor.stencilAttachment.clearStencil = 0
+//		renderPassDescriptor.stencilAttachment.texture = targets[1] // TODO: for loop.
+	}
+	
 	override func draw(in view: MTKView, by commandBuffer: MTLCommandBuffer) {
 		// TODO: safety skip all.
 		// TODO: customize this function varying from render passes.
-		guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+//		guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+//		print(renderPassDescriptor)
+		
 		guard let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else { return }
 		
 		// Start encoding and setup debug infomation
@@ -47,9 +79,5 @@ class MainRenderPass: RenderPass {
 		
 		// End encoding.
 		renderCommandEncoder.endEncoding()
-		
-		// TODO: render target.
-		// If rendering to core animation layer.
-		if let drawable = view.currentDrawable { commandBuffer.present(drawable) }
 	}
 }
