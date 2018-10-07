264327c2a4b7ad9fe4d066658522c0de389b5239
HaijianLiu <haijanliu@gmail.com>
2018-10-07 23:11:06 +0900

    Add blit view target functions.

--- Phantom/Graphics/RenderPass.swift
+++ Phantom/Graphics/RenderPass.swift
@@ -5,11 +5,11 @@ import MetalKit
 class RenderPass: Drawable, Registrable {
 	// TODO: use texture class.
 	// TODO: color attachments dictionary.
-	//TODO: double textures for asyc render?
+	// TODO: double textures for asyc render?
 	var targets = [MTLTexture]()
 	
-	var viewColor: MTLTexture?
-	var viewDepth: MTLTexture?
+	private var viewColor: MTLTexture?
+	private var viewDepth: MTLTexture?
 	
 	var isViewDirty = false
 	
@@ -33,4 +33,59 @@ class RenderPass: Drawable, Registrable {
 	func register() { }
 	
 	func draw(in view: MTKView, by commandBuffer: MTLCommandBuffer) { }
+	
+	func blitViewColor(in view: MTKView, by blitCommandEncoder: MTLBlitCommandEncoder, from texture: MTLTexture, mipmapped: Bool = false) -> MTLTexture? {
+		// Dispath init view textures.
+		guard let color = viewColor else {
+			DispatchQueue.global(qos: .background).async {
+				let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: texture.pixelFormat, width: texture.width, height: texture.height, mipmapped: mipmapped)
+				self.viewColor = view.device?.makeTexture(descriptor: descriptor)
+				self.isViewDirty = false
+			}
+			blitCommandEncoder.endEncoding()
+			return nil
+		}
+		
+		// Encode blit command.
+		if !isViewDirty && texture.width == color.width && texture.height == color.height {
+			let origin = MTLOrigin(x: 0, y: 0, z: 0)
+			let size = MTLSize(width: texture.width, height: texture.height, depth: texture.depth)
+			blitCommandEncoder.copy(from: texture, sourceSlice: 0, sourceLevel: 0, sourceOrigin: origin, sourceSize: size, to: color, destinationSlice: 0, destinationLevel: 0, destinationOrigin: origin)
+			if mipmapped {
+				blitCommandEncoder.generateMipmaps(for: color)
+			}
+		} else {
+			blitCommandEncoder.endEncoding()
+			viewColor = nil
+			isViewDirty = true
+		}
+		
+		return viewColor
+	}
+	
+	func blitViewDepth(in view: MTKView, by blitCommandEncoder: MTLBlitCommandEncoder, from texture: MTLTexture) -> MTLTexture? {
+		// Dispath init view textures.
+		guard let depth = viewDepth else {
+			DispatchQueue.global(qos: .background).async {
+				let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: texture.pixelFormat, width: texture.width, height: texture.height, mipmapped: false)
+				descriptor.storageMode = .private
+				self.viewDepth = view.device?.makeTexture(descriptor: descriptor)
+				self.isViewDirty = false
+			}
+			blitCommandEncoder.endEncoding()
+			return nil
+		}
+		
+		// Encode blit command.
+		if !isViewDirty && texture.width == depth.width && texture.height == depth.height {
+			let origin = MTLOrigin(x: 0, y: 0, z: 0)
+			let size = MTLSize(width: texture.width, height: texture.height, depth: texture.depth)
+			blitCommandEncoder.copy(from: texture, sourceSlice: 0, sourceLevel: 0, sourceOrigin: origin, sourceSize: size, to: depth, destinationSlice: 0, destinationLevel: 0, destinationOrigin: origin)
+		} else {
+			blitCommandEncoder.endEncoding()
+			viewDepth = nil
+		}
+		
+		return viewDepth
+	}
 }
