c21c11b2849876e7c55bc9c2924d6b49cba2d7e7
HaijianLiu <haijanliu@gmail.com>
2018-10-18 23:33:56 +0900

    Add new files.

---
+++ PhantomTouchKit/PhantomTouchKit.h
@@ -0,0 +1,13 @@
+// Copyright © haijian. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhantomTouchKit.
+FOUNDATION_EXPORT double PhantomTouchKitVersionNumber;
+
+//! Project version string for PhantomTouchKit.
+FOUNDATION_EXPORT const unsigned char PhantomTouchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhantomTouchKit/PublicHeader.h>
+
+
